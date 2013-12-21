de864ba5633fa91b2a74589bc0d964771b2f2a4f
Souvatzis Vasilios <vasouv@gmail.com>
2013-12-22 00:33:24 +0200

    Initial commit and obj class

---
+++ src/dietonjava/MainGUIController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dietonjava;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author vasouv
+ */
+public class MainGUIController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
